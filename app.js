06d198324259805d7d4f8cc42f8741854726e9f2
Anurag41682 <anuragyadav41682@gmail.com>
2022-12-22 21:12:00 +0530

    Reuploading

--- app.js
+++ app.js
@@ -1,123 +1,140 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose=require("mongoose");
-const _=require("lodash");
-const { redirect } = require("express/lib/response");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+const {
+    redirect
+} = require("express/lib/response");
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(express.static("public"));
-const uri="mongodb+srv://anurag2002:25092002%40aA@cluster0.rbg9i.mongodb.net/todolistDB";
+const uri = "mongodb+srv://anurag2002:25092002%40aA@cluster0.rbg9i.mongodb.net/todolistDB";
 mongoose.connect(uri);
 
-const itemsSchema={
-    name:String,
+const itemsSchema = {
+    name: String,
 }
-const Item=mongoose.model("Item",itemsSchema);
-const listSchema={
-    name:String,
-    item:[itemsSchema]
+const Item = mongoose.model("Item", itemsSchema);
+const listSchema = {
+    name: String,
+    item: [itemsSchema]
 }
-const List=mongoose.model("List",listSchema);
-const firstItem=new Item({
-    name:"Welcome to your To do list"
+const List = mongoose.model("List", listSchema);
+const firstItem = new Item({
+    name: "Welcome to your To do list"
 });
-const secondItem=new Item({
-    name:"hit + to add new item"
+const secondItem = new Item({
+    name: "hit + to add new item"
 });
-const thirdItem=new Item({
-    name:"<--hit here to delete item"
+const thirdItem = new Item({
+    name: "<--hit here to delete item"
 });
-const defaultItem=[firstItem,secondItem,thirdItem];
+const defaultItem = [firstItem, secondItem, thirdItem];
 
 app.get("/", function(req, res) {
-    Item.find(function(err,result){
-        if(result.length===0)
-        {
-            Item.insertMany(defaultItem,function(err){
-                if(!err){
+    Item.find(function(err, result) {
+        if (result.length === 0) {
+            Item.insertMany(defaultItem, function(err) {
+                if (!err) {
                     res.redirect("/");
                 }
             });
-        }else{
+        } else {
             res.render("list", {
                 listTitle: "Today",
                 newListItems: result
             });
         }
-    });  
+    });
 });
-app.post("/", function(req, res){
+app.post("/", function(req, res) {
 
     const item = req.body.newItem;
-    const newitem=new Item({
-        name:item
+    const newitem = new Item({
+        name: item
     });
-    if(req.body.list==="Today")
-    {
-        newitem.save();  
+    if (req.body.list === "Today") {
+        newitem.save();
         res.redirect("/");
-    }else{
-        List.findOne({name:req.body.list},function(err,foundlist){  
+    } else {
+        List.findOne({
+            name: req.body.list
+        }, function(err, foundlist) {
             foundlist.item.push(newitem);
-            foundlist.save(function(){
-                res.redirect("/"+req.body.list);
-            });            
-        });        
-    }    
+            foundlist.save(function() {
+                res.redirect("/" + req.body.list);
+            });
+        });
+    }
 });
-app.post("/delete",function(req,res){
-    const checkedItemID=req.body.checkbox;
-    const listName=req.body.listName;
-    if(listName==="Today"){
-        Item.findByIdAndRemove(checkedItemID,function(err){
-            if(err){
+app.post("/delete", function(req, res) {
+    const checkedItemID = req.body.checkbox;
+    const listName = req.body.listName;
+    if (listName === "Today") {
+        Item.findByIdAndRemove(checkedItemID, function(err) {
+            if (err) {
                 console.log(err);
-            }else{
+            } else {
                 console.log("Success");
             }
         });
         res.redirect("/");
-    }else{
-        List.findOneAndUpdate({name:listName},{$pull:{item:{_id:checkedItemID}}},{new:true},function(err,result){
-            if(!err){
-                res.redirect("/"+listName); 
+    } else {
+        List.findOneAndUpdate({
+            name: listName
+        }, {
+            $pull: {
+                item: {
+                    _id: checkedItemID
+                }
             }
-        });   
-    }    
+        }, {
+            new: true
+        }, function(err, result) {
+            if (!err) {
+                res.redirect("/" + listName);
+            }
+        });
+    }
 });
 
-app.get("/:customListName",function(req,res){
-    
-    const customListName= _.capitalize(req.params.customListName);
-    List.findOne({name:customListName},function(err,foundlist){
-        if(err){
+app.get("/:customListName", function(req, res) {
+
+    const customListName = _.capitalize(req.params.customListName);
+    List.findOne({
+        name: customListName
+    }, function(err, foundlist) {
+        if (err) {
             console.log(err);
-        }else{
-            if(!foundlist){
-                const list=new List({
-                    name:customListName,
-                    item:defaultItem
-                }); 
-                list.save(function(){
-                    res.redirect("/"+customListName);
-                });                
-            }else{
-                res.render("list",{
-                    listTitle:customListName,
-                    newListItems:foundlist.item 
+        } else {
+            if (!foundlist) {
+                const list = new List({
+                    name: customListName,
+                    item: defaultItem
+                });
+                list.save(function() {
+                    res.redirect("/" + customListName);
+                });
+            } else {
+                res.render("list", {
+                    listTitle: customListName,
+                    newListItems: foundlist.item
                 });
             }
         }
-    });    
+    });
 });
 
-app.get("/about", function(req, res){
-  res.render("about");
+app.get("/about", function(req, res) {
+    res.render("about");
 });
 
-app.listen(process.env.PORT || 3000, function() {
-  console.log("Server started on port 3000");
+const port = process.env.port || 3000;
+app.listen(port, function() {
+    console.log("Server started on port 3000");
 });
